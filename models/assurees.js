7a0b836dd7b9b90c336c1d9f55dcdbd64c8ca7d6
fritz <fritzjeune@outlook.com>
2020-02-28 00:03:34 -0500

    added enterprise Schema and Assurees Schema

--- models/assurees.js
+++ models/assurees.js
@@ -3,11 +3,45 @@
 const mongoose = require('mongoose');
 
 const AssureeSchema = new mongoose.Schema({
-    name: {
+    idNumber: {
+        type: number,
+        required: [true, 'S il vous plait, veuillez ajouter votre CIN/NIF'],
+        unique: true,
+        maxlength: [17, 'le numero d identification ne peut pas avoir plus de 17 chiffres.' ],
+        minlength: [10, 'le numero d identification ne peut pas avoir moins de 10 chiffres.'],
+        trim: true
+    },
+    surname: {
         type: String,
-        require: [true, 'please add the assuree name'],
+        required: [true, 'S il vous plait, veuillez ajouter le nom de famille'],
         unique: false,
         trim: true
-
-    }
+    },
+    lastname: {
+        type: String,
+        required: [true, 'S il vous plait, veuillez ajouter le prenom'],
+        unique: false,
+        trim: true
+    },
+    sexe: {
+        type: String,
+        required: true,
+        enum: [
+            'Masculin',
+            'Feminin'
+          ]
+    },
+    birthday: {
+        type: Date,
+        required: true
+    },
+    birthPlace: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        maxlength: [20, 'Phone number can not be longer than 20 characters']
+    },
+    
 });
