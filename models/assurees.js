4097eec816c11cd53bc677adf541231dcacfd982
Fritz R. Jeune <fritzjeune@outlook.com>
2020-03-02 12:43:21 -0800

    adding some models

--- models/assurees.js
+++ models/assurees.js
@@ -18,7 +18,7 @@ const AssureeSchema = new mongoose.Schema({
         trim: true
     },
     lastname: {
-        type: String,
+        type: String, 
         required: [true, 'S il vous plait, veuillez ajouter le prenom'],
         unique: false,
         trim: true
@@ -36,12 +36,57 @@ const AssureeSchema = new mongoose.Schema({
         required: true
     },
     birthPlace: {
+        city: String,
+        province: String,
+        state: String,
+        country: String
+    },
+    maritalStatus: {
         type: String,
-        required: true,
+        enum: ['Celibataire', 'Marié(e)', 'Divorcé(e)', 'Veuf(ve)', 'Union Libree', 'Séparé(e)'],
+    },
+    matrimoniaProperty: {
+        type: String,
+        enum: ['Communauté', 'Séparation']
+    },
+    //contact part
+    address: {
+        street: String,
+        city: String,
+        province: String,
+        state: String,
+        country: String
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     phone: {
         type: String,
-        maxlength: [20, 'Phone number can not be longer than 20 characters']
+        maxlength: [12, 'Phone number can not be longer than 12 characters']
+    },
+    //family details
+    members: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'PresentWorks',
+        required: false  
+    },
+    //studies 
+    educationLevel: {
+        required: false,
+        type: String,
+        enum: ['c0', 'c1', 'c2', 't1', 'u1', 'u2','u3']
+    },
+    profession: String,
+    //Presents works
+    presentWorks: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'PresentWorks',
+        required: false
     },
     
 });
+
+module.exports = AssureeSchema;
--- models/subtables.js
+++ models/subtables.js
@@ -1,5 +1,20 @@
 // jshint esversion:9
 
+const mongoose = require('mongoose');
+
+const famMemberSchema = new mongoose.Schema({
+    relation: String,
+    lastName: String,
+    firstName: String,
+    birthday: Date,
+    alive: Boolean,
+    deathDate: Date   
+});
+
+
+module.exports = famMemberSchema;
+
+
 const presentWorks = new mongoose.Schema({
     businessName: String,
     identificationNumber: Number,
